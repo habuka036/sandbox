c89f3c51708ed84dd0dc15b5d72fdf7182ab5cd0
habuka036 <osamu@habuka.jp>
2021-12-23 05:02:52 +0900

    export

--- inNDays.js
+++ inNDays.js
@@ -1,4 +1,4 @@
-function inNDaysWithoutHoliday(id, n) {
+export function inNDaysWithoutHoliday(id, n) {
 	import { isHoliday } from "https://code4fukui.github.io/day-es/Holiday.js";
 	console.log(isHoliday(new Day("2021-07-22"))); // true
 }
